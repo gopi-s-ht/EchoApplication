fffa5d0e24d027df55b6344745bf940e5df85dec
gopi-s-ht <gopi.s@hoonartek.com>
2024-01-02 14:17:24 +0530

    Postgresql setup

--- web_server_init.sh
+++ web_server_init.sh
@@ -1,4 +1,10 @@
 sudo apt-get update
+#web server setup
 sudo apt-get install apache2 -y
 sudo service apache2 start
 echo "Hello world!" > /var/www/html/index.html
+#Postgresql setup
+sudo apt install postgresql -y
+sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /etc/postgresql/9.5/main/postgresql.conf
+sudo sh -c 'echo "host    all             all             0.0.0.0/0                trust" >> /etc/postgresql/9.5/main/pg_hba.conf'
+sudo service postgresql restart
